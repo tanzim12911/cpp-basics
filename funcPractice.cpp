c587d8d9a46090b13e42f6b43bff9997cc519a13
tanzim12911 <tanzim12911@gmail.com>
2024-01-04 05:39:30 +0000

    Jan 4, 2024, 11:39 AM

--- funcPractice.cpp
+++ funcPractice.cpp
@@ -7,10 +7,14 @@ using namespace std;
 
 const double SALES_TAX = 0.075;
 
+double computeTax(double subTotal);
+
 int main() 
 {
     double subTotal = 10.0;
     double total = subTotal + computeTax(subTotal);
+
+    cout << "Total: " << total << endl;
 }
 
 double computeTax(double subTotal) {
