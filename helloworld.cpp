c9ee8c917f84cd3ae668c5015a89e500b64bbdb8
tanzim12911 <tanzim12911@gmail.com>
2024-07-01 17:23:19 +0000

    Jul 1, 2024, 11:23 PM

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,6 +6,15 @@ int main() {
         multiline code
     */
 
+   int x; // declaration
+   x = 5;
+
+   int y = 6;
+
+   std::cout << x;
+   std::cout << y;
+
+
     std::cout << "I like pizza!" << std::endl; // This is comment
     std::cout <<"It's really good!" << '\n';
     return 0;
