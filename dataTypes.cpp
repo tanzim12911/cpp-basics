0b2423a2a48733f8ac2c1b5900d4d2e8f227fc92
tanzim12911 <tanzim12911@gmail.com>
2024-07-01 17:33:06 +0000

    Jul 1, 2024, 11:33 PM

---
+++ dataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    //integer (whole number)
+    int age = 21;
+    int year = 2024;
+    int days = 7.5;         // 0.5 turnacated
+
+    //double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temeperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char initial = 'B';
+    
+}
