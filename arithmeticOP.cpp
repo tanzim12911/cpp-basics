d3db606ab0590b4cda0c968575905750ce72cc21
tanzim12911 <tanzim12911@gmail.com>
2024-07-01 18:45:18 +0000

    Jul 2, 2024, 12:45 AM

--- arithmeticOP.cpp
+++ arithmeticOP.cpp
@@ -5,10 +5,27 @@ int main() {
     // arithmetic operators = return the result of a specific
     //                        arithmetic operation (+ - * /)
 
+    /*
+        order : 1.parenthesis
+                2.multiplication & division
+                3.addition & subtraction
+    */
+
     int students = 20;
 
     //students = students + 1;
-    students += 2;
+    //students++;
+    //students += 2;
+    
+    //students--;
+
+    //students = students * 2;
+    //students *= 2;
+    
+    //students = students / 2;
+    //students /= 2
+
+    int remainder = students % 2;  // {mod (%) gives you remainder}
 
     std::cout << students << '\n';
 } 
