b95bdbc9bb506c71ada53bea2ab64a15ac8ee5b0
tanzim12911 <tanzim12911@gmail.com>
2024-07-01 18:36:27 +0000

    Jul 2, 2024, 12:36 AM

--- typedef.cpp
+++ typedef.cpp
@@ -3,13 +3,20 @@
 
 typedef std::vector<std::pair<std::string, int>> pairlist_t;
 typedef std::string text_t;
+//typedef int number_t;
+using number_t = int;
 
 int main() {
 
     // typedef = reserved keyword used to create an additional name
-    //           (alis) for another data type.
+    //           (alias) for another data type.
     //           New identifier for an exitsing type
     //           Helps with readablity and redces typos
+
+    /*
+        Use when there is a clear benefit
+        Replaced with 'using' (work better 2/ templates)
+    */
 
     
     //std::vector<std::pair<std::string, int>> pairlist;
@@ -18,7 +25,11 @@ int main() {
     //std::string firstName = "Bro";
     text_t firstName = "Bro";
 
-    std::cout << firstName;
+    std::cout << firstName << '\n';
+
+    number_t age = 21;
+
+    std::cout << firstName << " " << age << '\n';
 
     return 0; 
 }
