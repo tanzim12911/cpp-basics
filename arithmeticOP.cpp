6a9f67190338c68ccda8a7ba58f6ec533b8aa838
tanzim12911 <tanzim12911@gmail.com>
2024-07-01 18:39:32 +0000

    Jul 2, 2024, 12:39 AM

---
+++ arithmeticOP.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // arithmetic operators = return the result of a specific
+    //                        arithmetic operation (+ - * /)
+
+    int students = 20;
+
+    //students = students + 1;
+    students += 2;
+
+    std::cout << students << '\n';
+} 
