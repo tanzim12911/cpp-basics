0dca7791cd1341b056efb85a0a69ddd9d7d7a61b
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 10:42:11 +0000

    Jul 2, 2024, 4:42 PM

--- 11_ifStatements.cpp
+++ 11_ifStatements.cpp
@@ -11,6 +11,17 @@ int main() {
     std::cin >> age;
 
     if(age >= 18) {
-        std::cout << "Welcome to the site!";
+        std::cout << "Welcome to the site!";            // order of else if matter
     }
+    else if (age < 0) {
+        std::cout << "You haven't been born yet.";
+    }
+    else if (age >= 100) {
+        std::cout << "You are too old to enter this site.";
+    }
+    else {
+        std::cout << "You are not old enough to enter.";
+    }
+
+    return 0;
 }
