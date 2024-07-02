6ab2d4181ff53bcbbda9cd15440f1c0eff9a56d1
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 10:12:19 +0000

    Jul 2, 2024, 4:12 PM

--- 08_userInput.cpp
+++ 08_userInput.cpp
@@ -2,18 +2,18 @@
 
 // cout << {insertion operator}
 // cin >> {extraction operator} [can take string till space as input]
-// std::getline(std::cin, variable); [for taking string w/space]
+// std::getline(std::cin, variable); [for taking string w/whitespace]
 
 int main() {
 
     std::string name;
     int age;
 
-    std::cout << "What's your full name?: ";
-    std::getline(std::cin, name);
-
     std::cout << "What's your age? ";
     std::cin >> age;
+
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin, name);
 
     std::cout << "Hello " << name << '\n';
     std::cout << "You are " << age << " years old.";
