6abb15b08c86a045c1fb18a67bf464e345c94f1a
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 18:34:23 +0000

    Jul 3, 2024, 12:34 AM

---
+++ 26_userDefinedFunc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void happyBirthday() {
+    std::cout << "Happy Birthday to you!\n";
+    std::cout << "Happy Birthday to you!\n";
+    std::cout << "Happy Birthday to you!\n";
+}
+
+int main() {
+    // function = a block of reusable code
+
+    happyBirthday();
+}
