e7b02e881f1e9e401ed21bde580bd0714b76b98c
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 11:55:46 +0000

    Jul 2, 2024, 5:55 PM

--- 14_ternaryOP.cpp
+++ 14_ternaryOP.cpp
@@ -5,9 +5,15 @@ int main() {
     // ternary operator ?: = replacement to an if/else statement
     // condition ? expression1 : expression2;
 
-    int grade = 75;
+    /*
+        int grade = 75;
 
-    (grade >=60) ? std::cout << "You passed!" : std::cout << "Your FAILED!"; 
+        (grade >=60) ? std::cout << "You passed!" : std::cout << "Your FAILED!"; 
+    */
+
+   int number = 8;
+
+   (number % 2 == 0) ? std::cout << "Even" : std::cout << "Odd";
 
     return 0;
 }
