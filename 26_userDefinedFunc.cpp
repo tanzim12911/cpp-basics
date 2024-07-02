669720d46082b1cad6347668b0babb9519f69fb0
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 18:42:32 +0000

    Jul 3, 2024, 12:42 AM

--- 26_userDefinedFunc.cpp
+++ 26_userDefinedFunc.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-void happyBirthday(std::string name);
+void happyBirthday(std::string name, int age);
 
 int main() {
     
     // function = a block of reusable code
-
+    int age = 21;
     std::string name = "Bro";
 
-    happyBirthday(name);
+    happyBirthday(name, age);
 }
 
-void happyBirthday(std::string name) {
+void happyBirthday(std::string name, int age) {
     std::cout << "Happy Birthday to " << name << '\n';
     std::cout << "Happy Birthday to " << name << '\n';
     std::cout << "Happy Birthday dear " << name << '\n';
