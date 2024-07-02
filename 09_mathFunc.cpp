b6e1e5ed217d0467a3be32e7fc6b48917b941c3f
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 10:25:26 +0000

    Jul 2, 2024, 4:25 PM

--- 09_mathFunc.cpp
+++ 09_mathFunc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
     
@@ -7,7 +8,11 @@ int main() {
     double z;
 
     // z = std::max(x, y);
-    z = std::min(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4);
+    // z = sqrt(9);
+
+    z = abs(-3);
 
     std::cout << z;
 
