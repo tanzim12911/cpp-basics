98f964a7f6f1f6e3f02c89d9696fb8eeddf158d7
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 17:56:48 +0000

    Jul 2, 2024, 11:56 PM

--- 23_randomNumGen.cpp
+++ 23_randomNumGen.cpp
@@ -3,12 +3,18 @@
 int main() {
     
     // pseudo-random = NOT truly random (but close)
+    // intialize random number generator [srand(time(NULL));]
+    // rand() % last_num + first_num;
 
     srand(time(NULL));
 
-    int num = rand() % 6 + 1;
+    int num1 = rand() % 6 + 1;
+    int num2 = rand() % 6 + 1;
+    int num3 = rand() % 6 + 1;
 
-    std::cout << num;
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num2 << '\n';
 
     return 0;
 }
