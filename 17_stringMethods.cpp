7d62890a599f9d1b4d0f1f1daaab8158ff46f063
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 16:58:33 +0000

    Jul 2, 2024, 10:58 PM

--- 17_stringMethods.cpp
+++ 17_stringMethods.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// .lenght()
+// .empty()
+
 int main() {
 
     std::string name;
@@ -7,12 +10,17 @@ int main() {
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    if(name.length() > 12) {
+    if(name.empty()) {
+        std::cout << "You didn't enter your name\n";
+    }
+
+    else if(name.length() > 12) {
         std::cout << "Your name can't be over 12 characters";
     }
     else {
         std::cout << "Welcome " << name;
     }
+    
 
     return 0;
 
