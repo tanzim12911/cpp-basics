ede26d87e8eb91b2a17a838f1bf276686013b9aa
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 17:09:46 +0000

    Jul 2, 2024, 11:09 PM

--- 17_stringMethods.cpp
+++ 17_stringMethods.cpp
@@ -2,10 +2,13 @@
 
 int main() {
 
-    // .length()
-    // .empty()
-    // .clear()
-    // .at(index)
+    // .length()                    [Find string length]
+    // .empty()                     [Checks if string is empty or not]
+    // .clear()                     [Erases string] 
+    // .at(index)                   [Return the character at the index]
+    // .erase(index1, index2)       [Erased from, to index of the string]
+    // .append("string")            [appends]
+    // .find()                      [Returns index of the searched character]
 
     
     std::string name;
