0768673bd42a7393bc71819b77ea646772bbae59
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 12:09:38 +0000

    Jul 2, 2024, 6:09 PM

--- 15_logicalOP.cpp
+++ 15_logicalOP.cpp
@@ -7,24 +7,24 @@ int main() {
     // ! = reverse the logical state of its operand
 
     int temp;
-    bool sunny;
+    bool sunny = true;
 
     std::cout << "Enter the temperature: ";
     std::cin >> temp;
 
     if(temp <= 0 || temp >= 30) {
-        std::cout << "The temperature is bad";
+        std::cout << "The temperature is bad \n";
     }
     else {
-        std::cout << "The temperature is good";
+        std::cout << "The temperature is good\n";
     }
 
 
     if(sunny) {
-        std::cout << "It is sunny outside";
+        std::cout << "It is sunny outside\n";
     }
     else {
-        std::cout << "It is cloudy outside";
+        std::cout << "It is cloudy outside\n";
     }
 
 
