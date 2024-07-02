8aa17271a58d01dddc80cfc622cca8c50762180b
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 09:56:22 +0000

    Jul 2, 2024, 3:56 PM

---
+++ 07_typeConversion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // type conversion = conversion a value of one data type to another
+    //                      Implicit = automatic
+    //                      Explicit = Precede value with new data type (int)
+
+    double x = (int) 3.14;
+
+    std::cout << x;
+
+    return 0;
+}
