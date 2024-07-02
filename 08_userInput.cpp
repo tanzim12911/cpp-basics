f6dd2105671abe09cbdfb2893a20641914236459
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 10:19:39 +0000

    Jul 2, 2024, 4:19 PM

--- 08_userInput.cpp
+++ 08_userInput.cpp
@@ -10,11 +10,13 @@ int main() {
     int age;
 
     std::cout << "What's your age? ";
-    std::cin >> age;
-
-    std::cout << "What's your full name?: ";
-    std::getline(std::cin, name);
+    std::cin >> age;                                            // when we use cin followed by getline, there's a '\n' character in 
+                                                                // the input buffer and we dont pick the character so when we get to
+    std::cout << "What's your full name?: ";                    // getline, it accepts the '\n' thats still within the buffer
+    std::getline(std::cin >> std::ws, name);                    // so we add [>> std::ws] after cin
 
     std::cout << "Hello " << name << '\n';
     std::cout << "You are " << age << " years old.";
+
+    return 0;
 }
