7bc112b2f221a2891a762acab09a0a863f009786
tanzim12911 <tanzim12911@gmail.com>
2024-07-02 10:53:07 +0000

    Jul 2, 2024, 4:53 PM

--- 12_switchCase.cpp
+++ 12_switchCase.cpp
@@ -5,48 +5,29 @@ int main() {
     // switch = alternative to using many "else if" statements
     //          compare one value against matching cases.
 
-    int month;
-    std::cout << "Enter the month (1-12): ";
-    std::cin >> month;
+    char grade;
+    
+    std::cout << "What letter grade?: ";
+    std::cin >> grade;
 
-    switch(month) {
-        case 1: 
-            std::cout << "It is January";
+    switch(grade) {
+        case 'A':
+            std::cout << "You did great!";
             break;
-        case 2: 
-            std::cout << "It is February";
+        case 'B':
+            std::cout << "You did good!";
             break;
-        case 3: 
-            std::cout << "It is March";
+        case 'C':
+            std::cout << "You did okay!";
             break;
-        case 4: 
-            std::cout << "It is April";
+        case 'D':
+            std::cout << "You did not do good!";
             break;
-        case 5: 
-            std::cout << "It is May";
-            break;
-        case 6: 
-            std::cout << "It is June";
-            break;
-        case 7: 
-            std::cout << "It is July";
-            break;
-        case 8: 
-            std::cout << "It is August";
-            break;
-        case 9: 
-            std::cout << "It is September";
-            break;
-        case 10: 
-            std::cout << "It is October";
-            break;
-        case 11: 
-            std::cout << "It is November";
-            break;
-        case 12: 
-            std::cout << "It is December";
+        case 'F':
+            std::cout << "You FAILED!";
             break;
         default:
-            std::cout << "Please enter in only number (1-12)";
+            std::cout << "Please enter only in letter grade (A-F)";
+        
     }
 }
