158c3362888bc39c53daf683613b6de1fab4e45c
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 17:05:13 +0000

    Jul 3, 2024, 11:05 PM

--- 28_overloadedFunc.cpp
+++ 28_overloadedFunc.cpp
@@ -5,9 +5,10 @@ void bakePizza(std::string topping1);
 
 int main() {
     
-    // Functioins with same name but different parameters
+    // Functions can have same name, but different parameters
 
     bakePizza("pepperoni");
+    bakePizza("pepperoni", "mushroom");
 }
 
 void bakePizza() {
@@ -16,4 +17,8 @@ void bakePizza() {
 
 void bakePizza(std::string topping1) {
     std::cout << "Here is your pizza " << topping1 << " pizza!\n";
+}
+
+void bakePizza(std::string topping1, std::string topping2) {
+    std::cout << "Here is your pizza " << topping1 << " and " << topping2 << " pizza!\n";
 }
