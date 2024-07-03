a696e55582bfe57c82f5fe40febc4794eb56e4c8
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 17:08:34 +0000

    Jul 3, 2024, 11:08 PM

---
+++ 29_variableScope.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    // Local variables = declared inside a function or block {}
+    // Global variables = declared outside of all functions
+
+    int myNum = 1;
+
+    std::cout << myNum;
+}
