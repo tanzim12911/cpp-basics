c3d6c0bfa0e316d30c92bdb5c95ef76b8d403e4c
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 17:00:39 +0000

    Jul 3, 2024, 11:00 PM

--- 27_returnKeyword.cpp
+++ 27_returnKeyword.cpp
@@ -2,6 +2,8 @@
 
 double square(double length);
 double cube(double length);
+std::string concatStrings(std::string string1, std::string string2);
+
 
 int main() {
 
@@ -15,6 +17,13 @@ int main() {
     std::cout << "Area: " << area << " cm^2\n";
     std::cout << "Volume: " << volume << " cm^3\n";
 
+    std::string firstName = "Bro";
+    std::string lastName = "Code";
+    std::string fullName = concatStrings(firstName, lastName);
+
+    std::cout << "Hello " << fullName << '\n';
+
+
     return 0;
 }
 
@@ -24,4 +33,8 @@ double square(double length) {
 }
 double cube(double length) {
     return length * length * length;
+}
+
+std::string concatStrings(std::string string1, std::string string2) {
+    return string1 + " " + string2;
 }
