4b4493906a20da34f1871349941473d5b88be8ad
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 17:41:04 +0000

    Jul 3, 2024, 11:41 PM

---
+++ 30_bankingPracticeProgram.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+
+    // A banking program : deposit money
+    //                     withdraw money and show balance
+
+    double balance = 0;
+    int choice = 0;
+
+    do
+    {
+        std::cout << "*******************\n";
+        std::cout << "Enter your choice: \n";
+        std::cout << "*******************\n";
+        std::cout << "1. Show Balance: \n";
+        std::cout << "2. Deposit Money: \n";
+        std::cout << "3. Withdraw Money: \n";
+        std::cout << "4. Exit \n";
+
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            showBalance(balance);
+            break;
+        case 2:
+            balance += deposit();
+            break;
+        case 3:
+            balance -= withdraw(0);
+            break;
+        case 4:
+            std::cout << "Thanks for visiting\n";
+            break;
+        default:
+            std::cout << "Invalid choice\n";
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
+void showBalance(double balance) {
+    std::cout << "Your balance is $" << std::setprecision(2) << std::fixed << balance << '\n';
+}
+double deposit() {
+    return 0;
+}
+double withdraw(double balance){
+    return 0;
+}
