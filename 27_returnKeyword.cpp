821ad47438dfde5442d8069c5b4bada5686a4b4d
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 16:56:35 +0000

    Jul 3, 2024, 10:56 PM

---
+++ 27_returnKeyword.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+double square(double length);
+double cube(double length);
+
+int main() {
+
+    // return = retun a value back to spot
+    //          where you called the encompassing function
+
+    double length = 6.0;
+    double area = square(length);
+    double volume = cube(length);
+
+    std::cout << "Area: " << area << " cm^2\n";
+    std::cout << "Volume: " << volume << " cm^3\n";
+
+    return 0;
+}
+
+double square(double length) {
+    
+    return length * length;
+}
+double cube(double length) {
+    return length * length * length;
+}
