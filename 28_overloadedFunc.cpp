bd8e621a34ecfe97f5d79d53be9cb57faaba2482
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 17:06:54 +0000

    Jul 3, 2024, 11:06 PM

--- 28_overloadedFunc.cpp
+++ 28_overloadedFunc.cpp
@@ -2,6 +2,7 @@
 
 void bakePizza();
 void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string topping2);
 
 int main() {
     
@@ -16,9 +17,9 @@ void bakePizza() {
 }
 
 void bakePizza(std::string topping1) {
-    std::cout << "Here is your pizza " << topping1 << " pizza!\n";
+    std::cout << "Here is your " << topping1 << " pizza!\n";
 }
 
 void bakePizza(std::string topping1, std::string topping2) {
-    std::cout << "Here is your pizza " << topping1 << " and " << topping2 << " pizza!\n";
+    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!\n";
 }
