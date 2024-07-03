4364387cee41b85d16d898725b037b8f7ee2b58e
tanzim12911 <tanzim12911@gmail.com>
2024-07-03 17:15:47 +0000

    Jul 3, 2024, 11:15 PM

--- 29_variableScope.cpp
+++ 29_variableScope.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int myNum = 3;
+int myNum = 3;  // Better not to use GLOBAL variable as its pollute global namespace & less secure
 
 void printNum();
 
@@ -8,10 +8,13 @@ int main() {
 
     // Local variables = declared inside a function or block {}
     // Global variables = declared outside of all functions
+    // A function will use any LOCAL variable first before resorting to any GLOBAL variable
 
     int myNum = 1;
 
-    std::cout << myNum;
+    std::cout << myNum << '\n';
+
+    std::cout << ::myNum << '\n';       /// use scope-resolution operator to use global version
 
     printNum();
 }
